143573858f93dd9839cb66959c27c792cfeb672e
Victor Quintero <victor@qva.mx>
2026-02-18 15:22:11 -0600

    Add variable 'nombre' to return a name

--- ejercicio01.js
+++ ejercicio01.js
@@ -2,6 +2,8 @@
 // Modifica la siguiente función creando una nueva variable llamada nombre y asignándole tu nombre. La función debe retornar esta nueva variable que contiene tu nombre
 
 function obtenerNombre() {
+    let nombre = "Víctor Quintero";
+    return nombre;
     // Escribe tu código aquí
 }
 
